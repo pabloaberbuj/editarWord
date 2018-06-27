d83b5e29a2017ff4a14493587079f0f2c9cd97f2
pabloaberbuj <pabloaberbuj@gmail.com>
2018-06-27 11:16:27 -0300

    Update textos.cs

--- WinForm/WinForm/textos.cs
+++ WinForm/WinForm/textos.cs
@@ -11,10 +11,16 @@ namespace WinForm
 {
     public class Textos
     {
-        public static string encabezadoBEV(Plan plan)
+        public static string encabezadoBEV1(Plan plan) //cambiar tabulado
         {
-            return "Nombre: " + plan.nombre[0] + ", " + plan.nombre[1] + "\t HC: " + plan.ID + "\t" + DateTime.Today.ToShortDateString() + "\n etapa " + "etapaPONER" + " – " + "isoPoner" + " – Equipo " + plan.equipo;
+            return "Nombre: " + plan.nombre[0] + ", " + plan.nombre[1] + "\t HC: " + plan.ID + "\t" + DateTime.Today.ToShortDateString();
         }
+        
+        public static string encabezadooBEV(Plan plan) //centrado
+        {
+            return "\n etapa " + "etapaPONER" + " – " + "isoPoner" + " – Equipo " + plan.equipo;
+        }
+        
         public static string parametrosCampoTto(Campo campo)
         {
             string aux = "G=" + campo.gantry + ", Col=" + campo.col;
@@ -55,8 +61,28 @@ namespace WinForm
             return "Tratamiento tridimensional conformado de " plan.patologia;
         }
         
+        public static string axialInforme(Plan plan)
+        {
+            return "Corte axial en isocentro de la lesión y planificación del tratamiento en una etapa, con campos de radiación conformados. Las curvas representan niveles de dosis en \% donde el 100\% representa "
+                + (plan.dosisTotal/10).ToString() +"Gy, indicados con diferentes colores.";
+        }
         
-Tratamiento tridimensional conformado de 
+        public static string coronalSagitalInforme()
+        {
+            return "Se muestra las vistas coronal y sagital reconstruidas a partir de los cortes tomográficos axiales. Las líneas azules son la proyección de la conformación de la lesión en cada corte axial.";
+        }
+        
+        public static string tresDInforme(Plan plan)
+        {
+            return "Imagen tridimensional del tratamiento sobre un corte axial, en el que está representada la superficie de " + (plan.dosisTotal/10).ToString() + "Gy en entramado de color rosa que envuelve la lesión. Las líneas rectas representan diferentes incidencias de los campos conformados. La intersección de los órganos críticos con el isovolumen de referencia, dará lugar a los histogramas acumulativos de dosis.";
+        }
+        
+        public static string dvhInforme(Plan plan)
+        {
+            return "Histograma de distribución de dosis en los diferentes volúmenes delimitados en la tomografía.";
+        }
+        
+
 
     }
 }
